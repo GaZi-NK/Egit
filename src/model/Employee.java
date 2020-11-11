dd9bcca1b31826027b9c8e53804dacc00f82f269
GaZi-NK <redman.fox@icloud.com>
2020-11-11 22:00:40 +0900

    11/11

---
+++ src/model/Employee.java
@@ -0,0 +1,19 @@
+package model;
+
+//Employeeテーブルのレコードを表すクラス
+public class Employee {
+	private String id;
+	private String name;
+	private int age;
+
+	public Employee(String id, String name, int age) {
+		this.id = id;
+		this.name = name;
+		this.age = age;
+	}
+
+	public String getId() {return this.id;}
+	public String getName() {return this.name;}
+	public int getAge() {return this.age;}
+
+}
